d1dbd1601ae7a2e7226ce6c1774f35f0460c4283
Ankit Anand <ankit@myanatomy.in>
2023-01-02 17:13:11 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println("The product is :" + (a*b));
+    }
+}
